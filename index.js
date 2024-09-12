1e8859bfbc6635688930f6998575cbac50c2bc6b
VishnuNKrishnan <binaryboxtechnology@gmail.com>
2024-09-13 02:04:48 +0400

    Multi file handling in progress

--- index.js
+++ index.js
@@ -7,15 +7,28 @@ import updateMetaData from './utils/updateMetadata.js'
 
 createFolder('./songs/updated')
 
-const origFilePath = `./songs/Peysaamal (Mounam Sollum Vaarthaigal).mp3`
+// const origFilePath = `./songs/Peysaamal (Mounam Sollum Vaarthaigal).mp3`
 
-identifySong(origFilePath)
-    .then(result => {
-        console.log(`-- Match found!`)
-        console.log(`-- Song details: ${result.title} | From ${result.album} | By ${result.artists}`)
-        updateMetaData(origFilePath, `./songs/updated/${result.title} (${result.album}).mp3`, result)  // <TODO: Do not hardcode audio format>
-    })
-    .catch(error => {
-        console.error('-- Error identifying song:', error.status ? error.status : error)
-        console.log('-- Lookup failed')
-    })
+const files = [
+    'One Love.mp3',
+    'Usalampatti Penkutti (Gentleman).mp3'
+]
+
+const identifyAndUpdate = origFilePath => {
+    identifySong(origFilePath)
+        .then(result => {
+            console.log(`-- Match found!`)
+            console.log(`-- Song details: ${result.title} | From ${result.album} | By ${result.artists}`)
+            updateMetaData(origFilePath, `./songs/updated/${result.title} (${result.album}).mp3`, result)  // <TODO: Do not hardcode audio format>
+        })
+        .catch(error => {
+            console.error('-- Error identifying song:', error.status ? error.status : error)
+            console.log('-- Lookup failed')
+        })
+}
+
+files.forEach(async (file, i) => {
+    setTimeout(() => {
+        identifyAndUpdate(`./songs/${file}`)
+    }, i * 10000)
+})
